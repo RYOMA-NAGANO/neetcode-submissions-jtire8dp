1bd22dfef55b3cc2796f88411dcb678fe28a9070
RYOMA-NAGANO <naganoryoma@icloud.com>
2026-08-05 19:38:10 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = []
+        for i in range(2 * n):
+            res.append(nums[i % n])
+        return res
