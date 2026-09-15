5958292322231e6cc5cae6b9478a13996974210f
RYOMA-NAGANO <naganoryoma@icloud.com>
2026-09-15 10:55:44 -0400

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        occ = set()
+        for num in nums:
+            if num not in occ:
+                occ.add(num)
+            else:
+                occ.remove(num)
+        return list(occ)[0]
